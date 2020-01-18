cb53b05aae81de9f92ff949ccca98021fade0fbc
Bryn Heath <brynheath1@gmail.com>
2020-01-18 14:25:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,13 +38,7 @@
 def naming_system
 
 earth = {
-  :label => "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"} } },
-
-  :label => "Order"},
-
-   :label => "Family", sub_category: {label: "Genus",} },
-
-   :label => "Species",sub_category: nil}
+  :label => "Kingdom", sub_category: {label: "Phylum", sub_category: {label: "Class"} } }, "Order"}, "Family", sub_category: {label: "Genus",} },"Species",sub_category: nil}
         }
 
   # Remember:
