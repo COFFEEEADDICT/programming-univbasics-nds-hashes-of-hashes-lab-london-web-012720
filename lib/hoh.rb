73938286104b8169ec75953c9a870d00a3823968
Bryn Heath <brynheath1@gmail.com>
2020-01-18 15:07:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -39,12 +39,15 @@ def naming_system
 
 earth = {
   :label => "Kingdom",
-  :sub_category=> {label: "Phylum", :sub_category=> {label: "Class",:sub_category=>{label: "Order"}  }}
+  :sub_category=> {label: "Phylum", :sub_category=> {label: "Class",:sub_category=>{label: "Order",:sub_category=>{label: "Family", :sub_category=> {label: "Genus",:sub_category=>{label: "Species",sub_category: nil}} }}}
 
- #PORTION_2 => {label: "Order"},
- #PORTION_3 => {label: "Family", sub_category: {label: "Genus",} },
+
+
  #PORTION_4 => {label: "Species",sub_category: nil}
         }
+
+return earth
+end
 
   # Remember:
   #  Kingdom
@@ -56,5 +59,3 @@ earth = {
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-return earth
-end
